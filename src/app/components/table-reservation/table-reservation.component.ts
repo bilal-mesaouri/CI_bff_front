55f13e4cca0094962b2258cf88bf7127675b0319
bilal-mesaouri <bilmsr.bm@gmail.com>
2024-09-21 01:41:25 +0200

    adding http client + its demo

--- src/app/components/table-reservation/table-reservation.component.ts
+++ src/app/components/table-reservation/table-reservation.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-table-reservation',
@@ -8,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './table-reservation.component.css'
 })
 export class TableReservationComponent {
+  httpClient: HttpClient ;
+  serverLink: string = "http://localhost:3003/";
+  constructor(private http: HttpClient) { 
+    this.httpClient = http ;
+  }
 
+  ngOnInit(): void {
+    this.http.get<any[]>(this.serverLink+"dining/tables").pipe(
+      map(data => data.filter(item => item.number >5))
+    ).subscribe({
+      next: (response :any)=>{console.log(response);return response;},
+      error:(error:any) => { console.log("eroooooor",error); }
+    }
+    );
+
+  }
 }
