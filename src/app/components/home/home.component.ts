e1948f63c7e2200198f2731e1540033bbfbe454d
Yasmine Moussaoui <yasminemoussaoui@MacBook-Air-de-Yasmine.local>
2024-09-30 20:59:09 +0200

    home component latest version

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatButton} from '@angular/material/button';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { ImageModule } from 'primeng/image';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatButton,ButtonModule,ImageModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  constructor(private router: Router) {}
+
+  navigateToNextPage() {
+    this.router.navigate(['/customer-counter']);
+  }
 }
