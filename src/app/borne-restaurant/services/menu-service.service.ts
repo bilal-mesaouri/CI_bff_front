5d93ae04bec8e5b5a99d4075d463dbeaf3b66807
Yasmine Moussaoui <yasminemoussaoui@MacBook-Air-de-Yasmine.local>
2024-09-30 16:48:08 +0200

    modification fonction createTableorder

--- src/app/borne-restaurant/services/menu-service.service.ts
+++ src/app/borne-restaurant/services/menu-service.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class MenuServiceService {
 
-  private apiUrl = 'http://localhost:3003/tableOrders';
+  private apiUrl = 'http://localhost:3003/dining/add-command';
   constructor(private http: HttpClient) { }
 
   getAllItems(){
@@ -17,13 +18,10 @@ export class MenuServiceService {
   getItems(type: string) {
     return this.http.get(`http://localhost:3003/menu?type=${type}`);
   }
-
-  // Méthode pour envoyer une commande de table (tableNumber et customersCount)
-  createTableOrder(tableNumber: number, customersCount: number): Observable<any> {
-    const body = { tableNumber, customersCount };
-    console.log("createTableOrder");
+  createTableOrder(tablesNumber: number[], customersCount: number): Observable<any> {
+    const body = { tablesNumber, customersCount };
+    console.log(body);
     // Envoyer la requête POST avec HttpClient
     return this.http.post<any>(this.apiUrl, body);
   }
-
 }
