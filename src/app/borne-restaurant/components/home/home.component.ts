b2ce4a08b17a9a0e76578365a2801053e23b15b5
Yasmine Moussaoui <yasminemoussaoui@MacBook-Air-de-Yasmine.local>
2024-09-30 16:47:08 +0200

    home component modification

--- src/app/borne-restaurant/components/home/home.component.ts
+++ src/app/borne-restaurant/components/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButton} from '@angular/material/button';
 import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { ImageModule } from 'primeng/image';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule,MatButton],
+  imports: [MatCardModule,MatButton,ButtonModule,ImageModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
